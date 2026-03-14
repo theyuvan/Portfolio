4c33b4becb853de7edddaef2e98ccf0e3517b4fc
theyuvan <yuvan.r2005@gmail.com>
2026-03-14 23:46:50 +0530

    Define technologies data and skill assets

---
+++ lib/constants.ts
@@ -0,0 +1,66 @@
+export const technologies = [
+  {
+    name: "HTML 5",
+    icon: "/tech/html5.png",
+  },
+  {
+    name: "CSS 3",
+    icon: "/tech/css3.png",
+  },
+  {
+    name: "JavaScript",
+    icon: "/tech/javascript.png",
+  },
+  {
+    name: "React JS",
+    icon: "/tech/reactjs.png",
+  },
+  {
+    name: "Next.js",
+    icon: "/tech/next.png",
+  },
+  {
+    name: "Node.js",
+    icon: "/tech/Node.png",
+  },
+  {
+    name: "Express",
+    icon: "/tech/express.png",
+  },
+  {
+    name: "Python",
+    icon: "/tech/Python.png",
+  },
+  {
+    name: "Java",
+    icon: "/tech/java.png",
+  },
+  {
+    name: "Tailwind CSS",
+    icon: "/tech/tailwind.png",
+  },
+  {
+    name: "MySQL",
+    icon: "/tech/mysql.png",
+  },
+  {
+    name: "Blockchain",
+    icon: "/tech/blockchain.png",
+  },
+  {
+    name: "Solidity",
+    icon: "/tech/solidity.png",
+  },
+  {
+    name: "Git",
+    icon: "/tech/git.png",
+  },
+  {
+    name: "Figma",
+    icon: "/tech/figma.png",
+  },
+  {
+    name: "Vercel",
+    icon: "/tech/vercel.png",
+  },
+]
